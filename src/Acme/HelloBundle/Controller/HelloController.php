7c50bef39c8d0c32682fa7f2ecd1f6f03a0b3106
ewindt <a.samuylov@starlink.biz>
2015-04-23 16:54:44 +0300

    test

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -1,8 +1,9 @@
 <?php
 namespace Acme\HelloBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-class HelloController {
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class HelloController extends Controller{
 	public function indexAction ($name){
 		return $this->render("AcmeHelloBundle:Hello:index.html.twig", array('name' => $name));
 		//return new Response("<html><body>Hello,asd $name!</body></html>");
